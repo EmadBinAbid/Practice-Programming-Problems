91e24a9b3477722d8065331cfb18b4530f972321
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-09-12 01:24:16 +0500

    Question 02 completed.

--- Week 01/question-02.py
+++ Week 01/question-02.py
@@ -4,3 +4,11 @@ should be same as the lyrics in the link below. Use as many loops as possible. T
 the number of lines used for the solution. Less lines of code, more points.
 """""
 
+def song_lyrics():
+    for bottles in range(99, 2, -1):
+        print("%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n\n" % (bottles, bottles, bottles - 1))
+    print("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n")
+    print("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n\n")
+    print("No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
+
+song_lyrics()
